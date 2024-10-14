909289765d6c848ca4a19011e2a459c9e64144ff
Daniil Stepanenko <daniil.stepanenko@flant.com>
2024-10-14 21:50:16 +0300

    added BuildCommand method
    Signed-off-by: Daniil Stepanenko <daniil.stepanenko@flant.com>

--- gosx-notifier.go
+++ gosx-notifier.go
@@ -2,6 +2,7 @@ package gosxnotifier
 
 import (
 	"errors"
+	"fmt"
 	"net/url"
 	"os/exec"
 	"path/filepath"
@@ -11,21 +12,21 @@ import (
 type Sound string
 
 const (
-	Default Sound = "'default'"
-	Basso   Sound = "Basso"
-	Blow    Sound = "Blow"
-	Bottle  Sound = "Bottle"
-	Frog    Sound = "Frog"
-	Funk    Sound = "Funk"
-	Glass   Sound = "Glass"
-	Hero    Sound = "Hero"
-	Morse   Sound = "Morse"
-	Ping    Sound = "Ping"
-	Pop     Sound = "Pop"
-	Purr    Sound = "Purr"
-	Sosumi  Sound = "Sosumi"
-	Submarine  Sound = "Submarine"
-	Tink       Sound = "Tink"	
+	Default   Sound = "'default'"
+	Basso     Sound = "Basso"
+	Blow      Sound = "Blow"
+	Bottle    Sound = "Bottle"
+	Frog      Sound = "Frog"
+	Funk      Sound = "Funk"
+	Glass     Sound = "Glass"
+	Hero      Sound = "Hero"
+	Morse     Sound = "Morse"
+	Ping      Sound = "Ping"
+	Pop       Sound = "Pop"
+	Purr      Sound = "Purr"
+	Sosumi    Sound = "Sosumi"
+	Submarine Sound = "Submarine"
+	Tink      Sound = "Tink"
 )
 
 type Notification struct {
@@ -46,86 +47,92 @@ func NewNotification(message string) *Notification {
 }
 
 func (n *Notification) Push() error {
-	if supportedOS() {
-		commandTuples := make([]string, 0)
-
-		//check required commands
-		if n.Message == "" {
-			return errors.New("Please specifiy a proper message argument.")
-		} else {
-			commandTuples = append(commandTuples, []string{"-message", n.Message}...)
-		}
-
-		//add title if found
-		if n.Title != "" {
-			commandTuples = append(commandTuples, []string{"-title", n.Title}...)
-		}
-
-		//add subtitle if found
-		if n.Subtitle != "" {
-			commandTuples = append(commandTuples, []string{"-subtitle", n.Subtitle}...)
-		}
-
-		//add sound if specified
-		if n.Sound != "" {
-			commandTuples = append(commandTuples, []string{"-sound", string(n.Sound)}...)
-		}
-
-		//add group if specified
-		if n.Group != "" {
-			commandTuples = append(commandTuples, []string{"-group", n.Group}...)
-		}
-
-		//add appIcon if specified
-		if n.AppIcon != "" {
-			img, err := normalizeImagePath(n.AppIcon)
-
-			if err != nil {
-				return err
-			}
-
-			commandTuples = append(commandTuples, []string{"-appIcon", img}...)
-		}
-
-		//add contentImage if specified
-		if n.ContentImage != "" {
-			img, err := normalizeImagePath(n.ContentImage)
-
-			if err != nil {
-				return err
-			}
-			commandTuples = append(commandTuples, []string{"-contentImage", img}...)
-		}
-
-		//add url if specified
-		url, err := url.Parse(n.Link)
-		if err != nil {
-			n.Link = ""
-		}
-		if url != nil && n.Link != "" {
-			commandTuples = append(commandTuples, []string{"-open", n.Link}...)
-		}
-
-		//add bundle id if specified
-		if strings.HasPrefix(strings.ToLower(n.Link), "com.") {
-			commandTuples = append(commandTuples, []string{"-activate", n.Link}...)
-		}
-
-		//add sender if specified
-		if strings.HasPrefix(strings.ToLower(n.Sender), "com.") {
-			commandTuples = append(commandTuples, []string{"-sender", n.Sender}...)
-		}
-
-		if len(commandTuples) == 0 {
-			return errors.New("Please provide a Message and Type at a minimum.")
-		}
-
-		_, err = exec.Command(FinalPath, commandTuples...).Output()
-		if err != nil {
-			return err
-		}
+	cmd, err := n.BuildCommand()
+	if err != nil {
+		return err
 	}
-	return nil
+
+	return cmd.Run()
+}
+
+func (n *Notification) BuildCommand() (*exec.Cmd, error) {
+	if !supportedOS() {
+		return nil, fmt.Errorf("not supported")
+	}
+
+	commandTuples := make([]string, 0)
+
+	//check required commands
+	if n.Message == "" {
+		return nil, errors.New("please specify a proper message argument")
+	} else {
+		commandTuples = append(commandTuples, []string{"-message", n.Message}...)
+	}
+
+	//add title if found
+	if n.Title != "" {
+		commandTuples = append(commandTuples, []string{"-title", n.Title}...)
+	}
+
+	//add subtitle if found
+	if n.Subtitle != "" {
+		commandTuples = append(commandTuples, []string{"-subtitle", n.Subtitle}...)
+	}
+
+	//add sound if specified
+	if n.Sound != "" {
+		commandTuples = append(commandTuples, []string{"-sound", string(n.Sound)}...)
+	}
+
+	//add group if specified
+	if n.Group != "" {
+		commandTuples = append(commandTuples, []string{"-group", n.Group}...)
+	}
+
+	//add appIcon if specified
+	if n.AppIcon != "" {
+		img, err := normalizeImagePath(n.AppIcon)
+		if err != nil {
+			return nil, fmt.Errorf("normalize path %s: %w", n.AppIcon, err)
+		}
+
+		commandTuples = append(commandTuples, []string{"-appIcon", img}...)
+	}
+
+	//add contentImage if specified
+	if n.ContentImage != "" {
+		img, err := normalizeImagePath(n.ContentImage)
+		if err != nil {
+			return nil, fmt.Errorf("normalize path %s: %w", n.ContentImage, err)
+		}
+
+		commandTuples = append(commandTuples, []string{"-contentImage", img}...)
+	}
+
+	//add url if specified
+	url, err := url.Parse(n.Link)
+	if err != nil {
+		n.Link = ""
+	}
+	if url != nil && n.Link != "" {
+		commandTuples = append(commandTuples, []string{"-open", n.Link}...)
+	}
+
+	//add bundle id if specified
+	if strings.HasPrefix(strings.ToLower(n.Link), "com.") {
+		commandTuples = append(commandTuples, []string{"-activate", n.Link}...)
+	}
+
+	//add sender if specified
+	if strings.HasPrefix(strings.ToLower(n.Sender), "com.") {
+		commandTuples = append(commandTuples, []string{"-sender", n.Sender}...)
+	}
+
+	if len(commandTuples) == 0 {
+		return nil, errors.New("please provide a Message and Type at a minimum")
+	}
+
+	return exec.Command(FinalPath, commandTuples...), nil
 }
 
 func normalizeImagePath(image string) (string, error) {
